32cb9d094583e6ae702415e3a9da31dd5484f82c
Julian David <julianfullstackdev@gmail.com>
2023-08-22 19:26:18 -0500

    minor fixes

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -8,7 +8,7 @@ export default async function LandingLayout({
   return (
     <div className="flex">
       <div className="w-full md:w-8/12 bg-gradient-to-b from-zinc-900 to-zinc-800 text-white">
-        {children}
+        <main>{children}</main>
 
         <footer className="text-center pt-10 md:pt-20 pb-10 px-5">
           <span className="text-3xl font-bold">JD</span>
@@ -38,6 +38,7 @@ export default async function LandingLayout({
           href="https://github.com/hipdev/cv"
           target="_blank"
           rel="noopener noreferrer"
+          aria-label="Github repository"
           className="absolute hover:bg-black/90 transition-colors bg-black/80  rotate-45 w-40 flex justify-center -top-5 -right-14 h-20 items-end "
         >
           <AiFillGithub className="text-white text-4xl" />
